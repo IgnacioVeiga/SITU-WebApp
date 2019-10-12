0f40abbe2573388ce33b53c5e681c2fd38529b2a
Ignacio Veiga <ignacio.veiga29@gmail.com>
2019-10-12 04:44:45 -0300

    renombre

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tp6-bo app is running!');
+    expect(page.getTitleText()).toEqual('tp6-web app is running!');
   });
 
   afterEach(async () => {
