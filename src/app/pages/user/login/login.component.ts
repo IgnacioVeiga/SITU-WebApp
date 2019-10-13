9d555e43f96744a7f72352a0821c12366bfdc680
Jamil Uribe <juribe@ubicuagroup.com>
2019-10-12 23:13:37 -0300

    Update login

--- src/app/pages/user/login/login.component.ts
+++ src/app/pages/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  goTo(route: string) {
+    // recibe el path como string (ver app-routing.module)
+    this.router.navigate([route]);
+  }
 }
