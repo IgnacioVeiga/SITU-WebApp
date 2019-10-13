e35aa5fd30ab6cfe582e9ecdeb0ae5330a15ef43
Jamil Uribe <juribe@ubicuagroup.com>
2019-10-12 23:24:48 -0300

    Update register

--- src/app/pages/user/register/register.component.ts
+++ src/app/pages/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  goTo(route: string) {
+    // recibe el path como string (ver app-routing.module)
+    this.router.navigate([route]);
+  }
 }
