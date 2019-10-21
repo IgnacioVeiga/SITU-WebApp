3e60a20e4dd5a5c9481c020e00338fd7f7fa4754
Ignacio Veiga <ignacio.veiga29@gmail.com>
2019-10-21 19:27:07 -0300

    fix renombrar al español

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { InicioComponent } from './inicio/inicio.component';
 import { RegistrarseComponent } from './pages/usuario/registrarse/registrarse.component';
 import { MiPerfilComponent } from './pages/usuario/mi-perfil/mi-perfil.component';
 import { InfoComponent } from './pages/info/info.component';
-import { AyudaComponent } from './pages/ayuda/help.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 import { HorariosComponent } from './pages/horarios/horarios.component';
 import { HacerDenunciaComponent } from './pages/denuncias/hacer-denuncia/hacer-denuncia.component';
 import { ListaDenunciasComponent } from './pages/denuncias/lista-denuncias/lista-denuncias.component';
@@ -21,56 +21,47 @@ const routes: Routes = [
 
   // todos
   {
-    // inicio (todos)
     path: 'inicio',
     component: InicioComponent
   },
   {
-    // mi perfil (todos)
     path: 'mi-perfil',
     component: MiPerfilComponent
   },
   {
-    // ingresar (todos)
     path: 'ingresar',
     component: IngresarComponent
   },
 
   // pasajero
   {
-    // registrar (pasajero)
     path: 'registrarse',
     component: RegistrarseComponent
   },
   {
-    // denunciar (pasajero)
     path: 'hacer-denuncia',
     component: HacerDenunciaComponent
   },
   {
-    // información (pasajero)
     path: 'info',
     component: InfoComponent
   },
   {
-    // ayuda (pasajero)
     path: 'ayuda',
     component: AyudaComponent
   },
   {
-    // noticias o alertas (pasajero)
     path: 'noticias-alertas',
     component: NoticiasAlertasComponent
   },
   {
-    // horarios (pasajero)
     path: 'horarios',
     component: HorariosComponent
   },
 
   // admin
   {
-    // lista de incidencias (admin)
+    // lista de denuncias (admin)
     path: 'lista-denuncias',
     component: ListaDenunciasComponent
   },
