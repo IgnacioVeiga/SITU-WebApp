5cf89dfc41a2b59e204b31f54df0a4e2dd431694
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2023-07-25 19:54:13 -0300

    User list mockup (partial)

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['number', 'firstname', 'lastname', 'photo', 'role'];
+  dataSource: any = new MatTableDataSource<UserElement>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
+export interface UserElement {
+  number: number;
+  firstname: string;
+  lastname: string;
+  photo: string;
+  role: string;
+}
+
+const ELEMENT_DATA: UserElement[] = [
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+  { number: 0, firstname: 'Natalia', lastname: 'Natalia', photo: 'No disponible', role: 'Chofer' },
+];
