c4391d283978e16520f281cdc56fa0657d7e2a24
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2023-07-26 20:42:42 -0300

    "chips" for bus lines

--- src/app/pages/bus-routes/bus-routes.component.ts
+++ src/app/pages/bus-routes/bus-routes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-bus-routes',
@@ -8,12 +9,20 @@ import { Component } from '@angular/core';
 export class BusRoutesComponent {
   linea: number = 0;
   lineas: number[] = [];
-  trasbordos: string[] = ['Opción 1', 'Opción 2', 'Opción 3'];
+  trasbordos: string[] = ['Desde A - Hasta B', 'Desde B - Hasta A', 'Desde C - Hasta D', 'Desde D - Hasta C'];
 
-  addLinea() {
-    if (this.linea > 0) {
-      this.lineas.push(this.linea);
-      this.linea = 0;
+  addLinea(event: MatChipInputEvent): void {
+    const value = parseInt((event.value || '').trim());
+    if (value) {
+      this.lineas.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
+  removeLinea(linea: number) {
+    const index = this.lineas.indexOf(linea);
+    if (index >= 0) {
+      this.lineas.splice(index, 1);
     }
   }
 }
