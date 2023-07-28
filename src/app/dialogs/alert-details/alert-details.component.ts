57a09bd829d673ce72a95f16d0dd17dc092869c2
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2023-07-28 12:03:41 -0300

    alert details (using variables)

--- src/app/dialogs/alert-details/alert-details.component.ts
+++ src/app/dialogs/alert-details/alert-details.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./alert-details.component.scss']
 })
 export class AlertDetailsComponent {
+  title: string = '';
+  date: string = '';
+  generatedBy: string = '';
+  priorityLevel: string = '';
+  location: string = '';
+  description: string = '';
 
+  constructor() {
+    this.title = 'TÍTULO DE EJEMPLO';
+    this.date = '28/07/2023 12:00hs';
+    this.generatedBy = 'Pepe Argento';
+    this.priorityLevel = 'MEDIA';
+    this.location = 'Palermo - CABA';
+    this.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.';
+  }
 }
